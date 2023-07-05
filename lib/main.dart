fce4765ecd57d84a13186e71547bc39b3f367129
Feichtmeier <frederik.feichtmeier@gmail.com>
2023-07-05 17:53:29 +0200

    init on start

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatefulWidget {
 
   static Widget create(String apiKey) {
     return ChangeNotifierProvider<WeatherModel>(
-      create: (context) => WeatherModel(apiKey),
+      create: (context) => WeatherModel(apiKey)..init(),
       child: MyApp(apiKey: apiKey),
     );
   }
